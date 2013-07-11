089809795e02c7ac3b9ff957130d95da58b42308
harishgupta111@gmail.com <harishgupta111@gmail.com>
2013-07-11 18:58:12 +0530

    fixed failing tests

--- src/test/java/com/travel/agent/test/dao/VehicleMasterDaoTest.java
+++ src/test/java/com/travel/agent/test/dao/VehicleMasterDaoTest.java
@@ -9,7 +9,6 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
